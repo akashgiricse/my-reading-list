5a9d240a5b9a473111b6af31f727f8c94426f5eb
akashgiricse <akashgiricse@gmail.com>
2019-03-30 10:43:46 +0530

    Add Book schema and root query

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
+const graphqlHTTP = require('express-graphql');
+
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+
+
+
+}));
 
 app.listen(4000, () => {
 
---
+++ server/schema/schema.js
@@ -0,0 +1,26 @@
+const graphql = require('graphql')
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const BookType = new GraphQLObjectType({
+
+    name: 'Book',
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        genre: {type: GraphQLString}
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+   name: 'RootQueryType',
+    fields: {
+       book: {
+           type: BookType,
+           args: {id: {type: GraphQLString}},
+           resolve(parent, args){
+               // code to get data from db or other resources
+           }
+       }
+    }
+});
