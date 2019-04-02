6a4ec27cf149326d2c5005ac1554c5cbc4d1e461
akashgiricse <akashgiricse@gmail.com>
2019-04-02 07:57:31 +0530

    Two way relation between Author and books now

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,14 +6,18 @@ const {
     GraphQLString,
     GraphQLSchema,
     GraphQLID,
-    GraphQLInt} = graphql;
+    GraphQLInt,
+    GraphQLList} = graphql;
 
 // dummy data
 
 var books = [
     {name: 'Name of the wind', genre: 'Fantasy', id: '1', authorId: '1'},
     {name: 'The Final empire', genre: 'Fantasy', id: '2', authorId: '2'},
-    {name: 'The long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'}
+    {name: 'The long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
+    {name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
+    {name: 'The color of magic', genre: 'Fantasy', id: '5', authorId: '3'},
+    {name: 'The light Fantastic', genre: 'Sci-Fi', id: '6', authorId: '3'}
 
 ];
 
@@ -49,7 +53,13 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return _.filter(books,{ authorId: parent.id });
+            }
+        }
     })
 });
 
